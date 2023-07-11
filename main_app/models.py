d51fc9ebb802f3ef97a51b2bdd9ee2512a8923ad
shakiba333 <shakibaabdulrashid@gmail.com>
2023-07-11 11:53:25 -0400

    part two finch collector

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
-finches = [
-    {'name': 'Blue', 'gender': 'male',
-        'species': 'Gouldian finch', 'age': 2},
-    {'name': 'Sachi', 'gender': 'female',
-        'species': 'House Finch', 'age': 2},
-]
+from .models import Finch
+# finches = [
+#     {'name': 'Blue', 'gender': 'male',
+#         'species': 'Gouldian finch', 'age': 2},
+#     {'name': 'Sachi', 'gender': 'female',
+#         'species': 'House Finch', 'age': 2},
+# ]
 
 # Create your views here.
 
@@ -18,8 +18,14 @@ def about(request):
     return render(request, 'about.html')
 
 
-def finch_index(request):
+def finches_index(request):
+    finches = Finch.objects.all()
     # We pass data to a template very much like we did in Express!
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
